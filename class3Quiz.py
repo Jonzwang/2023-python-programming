93a40024b2500696e7af0f82cb4ed568d9393030
Jonzwang <jonathanzw5518@gmail.com>
2023-07-15 16:27:40 +0000

    add class3 quiz

---
+++ class3Quiz.py
@@ -0,0 +1,30 @@
+def my_sum(w, a, s, d):
+  ss = w + a + s + d
+  return (ss)
+
+
+w = 34
+a = 35
+s = 36
+d = 37
+print(f'The sum of {w}, {a}, {s} and {d} is {my_sum(w,a,s,d)}.')
+
+
+def my_multiplication(a, b):
+  c = a * b
+  return (c)
+
+
+a = 4325
+b = 9856
+print(f'The multiplication of {a} and {b} is {my_multiplication(a,b)}.')
+
+
+def my_division(a, b):
+  c = a / b
+  return (c)
+
+
+a = 9875867
+b = 768
+print(f'The division of {a} and {b} is {my_division(a,b)}.')
